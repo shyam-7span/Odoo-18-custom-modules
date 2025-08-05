2ca6f62db4a00883f9d11f732007f0e89fd14e3b
shyam-7span <shyam@7span.com>
2025-08-05 15:56:31 +0530

    [ADD]elearning_portal: Added new module.

---
+++ elearning_portal/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Module Name',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'Module Summary',
+    'description': '''Module Description''',
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
